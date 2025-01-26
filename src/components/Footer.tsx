10e2605a73918cdbd6b696b16216a81d72e8db12
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2025-01-26 12:33:36 -0600

    Remove the Red Hawk Robotics logo from the application

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,11 @@
 import { useQRScoutState } from '@/store/store';
 import { Heart } from 'lucide-react';
-import { Logo } from './Logo';
 
 export function Footer() {
   const teamNumber = useQRScoutState(state => state.formData.teamNumber);
   return (
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
-        <div className="h-24 w-96">
-          <Logo />
-        </div>
         {teamNumber !== 2713 && (
           <>
             <Heart className="text-primary size-8 fill-primary" />
