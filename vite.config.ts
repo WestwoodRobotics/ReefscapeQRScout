17eaefc2c8502d9c180e92fbd0b7eef37548334d
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2025-02-09 14:12:50 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // or remove this line
   plugins: [react()],
   resolve: {
     alias: {
