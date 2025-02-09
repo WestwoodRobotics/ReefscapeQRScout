71c32dd8d5c42a7f780fc40b4adb8e08151ff33e
Nikhil <nikhilb186199@gmail.com>
2025-02-09 14:10:03 -0600

    cooking

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  base: '/', // or remove this line
   plugins: [react()],
   resolve: {
     alias: {
